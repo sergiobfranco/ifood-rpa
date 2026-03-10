fb4dc2ccd84771da690953e540a456ba28552c29
Sérgio Borges Franco <sergiobfranco@gmail.com>
2026-03-10 12:10:42 -0300

    Nova porta 8571 padronizada

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@ set -e
 Xvfb :99 -screen 0 1280x1024x24 -ac &
 sleep 1
 
-# Inicia o Streamlit na porta 8570
+# Inicia o Streamlit na porta 8571
 exec streamlit run bot_streamlit.py \
-    --server.port=8570 \
+    --server.port=8571 \
     --server.address=0.0.0.0 \
     --server.headless=true \
     --browser.gatherUsageStats=false
